14279e35ef55531029df60a59c8ed4ad505ade17
Anders Schau Knatten <anders@knatten.org>
2012-06-19 20:17:11 +0100

    Helper scripts for switching between installed SnippetySnip and symlinks to development version

---
+++ remove_symlinks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm ~/.vim/plugin/SnippetySnip
+rm ~/.vim/python/SnippetySnip
---
+++ use_symlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR"/plugin/SnippetySnip" ~/.vim/plugin
+ln -s $DIR"/python/SnippetySnip" ~/.vim/python

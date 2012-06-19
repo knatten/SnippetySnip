497eeaa356241516d157d8ec1cdc526352145fba
Anders Schau Knatten <anders@knatten.org>
2012-06-19 19:54:53 +0100

    Also update date in set_version

--- set_version.sh
+++ set_version.sh
@@ -8,9 +8,11 @@ if [[ $# -ne 1 ]]; then
 fi
 
 VERSION_STRING="Version:     "$1
+DATE_STRING="Last change: "`date +%Y-%m-%d`
 SCRIPT=plugin/SnippetySnip/SnippetySnip.vim
 sed "s/Version:.*/$VERSION_STRING/" $SCRIPT > $SCRIPT".tmp"
-mv $SCRIPT".tmp" $SCRIPT
+sed "s/Last change:.*/$DATE_STRING/" $SCRIPT".tmp" > $SCRIPT
+rm $SCRIPT".tmp"
 
 echo "Tagged version "$1
 echo "Now commit and do"
